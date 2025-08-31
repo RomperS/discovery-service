6bad39fb323f2a1769f3c01f1782c7f7c2418846
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-08-31 02:09:03 +0000

    feat(cache): add Caffeine and enable caching

--- src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableCaching
 public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
